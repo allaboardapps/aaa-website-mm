37cadecb4c37ae83c3c1ec1fd140bd7945a4427b
Randy Burgess <wrburgess@gmail.com>
2018-02-20 11:30:58 -0600

    adding structured data and meta tags

--- Gemfile
+++ Gemfile
@@ -2,6 +2,8 @@ source "https://rubygems.org"
 
 ruby "2.4.3"
 
+gem "builder", "3.2.3"
 gem "middleman", "4.2.1"
 gem "middleman-autoprefixer", "2.9.0"
 gem "middleman-livereload", "3.4.6"
+gem "middleman-sprockets", "4.1.1"
--- config.rb
+++ config.rb
@@ -1,5 +1,4 @@
-# Activate and configure extensions
-# https://middlemanapp.com/advanced/configuration/#configuring-extensions
+# encoding: utf-8
 
 activate :autoprefixer do |prefix|
   prefix.browsers = "last 2 versions"
@@ -7,6 +6,26 @@ end
 
 # Layouts
 # https://middlemanapp.com/basics/layouts/
+
+# Global Variables
+set :site_description, "We develop and maintain software for small businesses and startups. Based in Chicago. Working for anywhere."
+set :site_keywords, "application, apps, software, development, design, Rails, JavaScript, React, Randy Burgess"
+set :site_name, "All Aboard Apps"
+set :author, "Randy Burgess"
+set :site_acronym, "AAA"
+set :site_email, "randy@allaboardapps.com"
+set :site_url, "https://www.allaboardapps.com"
+set :cdn_url, "https://s3.amazonaws.com/allaboardapps-website-assets"
+set :site_copyright, "© 2016-2018 All Aboard Apps. All Rights Reserved."
+set :image_url, "https://s3.amazonaws.com/allaboardapps-website-assets/full_color_with_text_1200x1200.png"
+set :ga_id, "UA-107700316-1"
+set :bing_validation, "x"
+set :yandex_validation, "x"
+set :facebook_url, "https://www.facebook.com/allaboardapps"
+set :linkedin_url, "https://https://www.linkedin.com/company/allaboardapps/"
+set :twitter_url, "https://twitter.com/allaboardapps"
+set :twitter_handle, "@allaboardapps"
+set :site_phone_number, "+1-773-551-6808"
 
 # Per-page layout changes
 page '/*.xml', layout: false
@@ -48,5 +67,12 @@ end
 activate :asset_hash
 activate :livereload
 activate :gzip
+activate :sprockets
 
-set(:port, 3000)
+if defined? RailsAssets
+  RailsAssets.load_paths.each do |path|
+    sprockets.append_path path
+  end
+end
+
+set(:port, 8080)
